330a2285aa361341dce113b758cf16a852280caa
Fanny <fannette.6576@gmail.com>
2019-10-18 12:20:04 +0200

    J'ai lier le raider.io a la guild

---
+++ src/components/CardGuild.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Guild extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roster: []
+    };
+  }
+}
+
+export default Guild;
--- src/pages/Guild.jsx
+++ src/pages/Guild.jsx
@@ -1,14 +1,53 @@
-import React from "react"
+import React from 'react';
+import axios from 'axios';
 
-import Navbar from "../components/Navbar"
+import Navbar from '../components/Navbar';
 
-function Guild() {
+class Guild extends React.Component {
+  constructor(props) {
+    console.log('Constructor');
+
+    super(props);
+
+    this.state = {
+      guildName: 'Method',
+      roster: [],
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    console.log('Component Did Mount');
+    this.setState({ isLoading: true });
+    axios
+      .get(
+        'https://raider.io/api/v1/raiding/raid-rankings?raid=uldir&difficulty=mythic&region=world'
+      )
+      .then(response => {
+        const ranking = response.data.raidRankings;
+        this.setState({ roster: ranking, isLoading: false });
+      });
+  }
+
+  render() {
+    console.log('Render');
+
     return (
-        <div>
-            <Navbar />
-            <h1>Guild</h1>
-        </div>
+      <div>
+        <Navbar />
+        {this.state.isLoading && <div>Loading...</div>}
+        <h1>{this.state.guildName}</h1>
+        {this.state.roster.map(guild => {
+          return (
+            <div>
+              {guild.rank}
+              {guild.guild.name}
+            </div>
+          );
+        })}
+      </div>
     );
+  }
 }
 
-export default Guild
+export default Guild;
