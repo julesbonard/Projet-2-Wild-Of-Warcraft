0422381d76f4d4e89855bc2845203d85e723d77d
Clara Desperben <cdesperben@yahoo.fr>
2019-10-18 17:42:56 +0200

    did modifications about the ranked guilds list, to only have the first three guilds of the raid

--- src/components/CardHome.jsx
+++ src/components/CardHome.jsx
@@ -1,60 +1,68 @@
 import React from 'react';
 import { CardImg } from 'reactstrap';
-import axios from "axios"
+import axios from 'axios';
 
-import StyledCardHome from './StyleCardHome'
-import "../Css/CardHome.css"
+import StyledCardHome from './StyleCardHome';
+import '../Css/CardHome.css';
 
 class CardHome extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            ranking: [],
-            isError: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      ranking: [],
+      isError: false
+    };
+  }
 
-    componentDidMount() {
-        axios.get(`https://raider.io/api/v1/raiding/raid-rankings?raid=${this.props.raid}&difficulty=mythic&region=world
-        `)
-            .then(res => {
-                const datas = res.data.raidRankings
-                this.setState({ ranking: datas })
-            })
-            .catch(err => {
-                console.log(err)
-                this.setState({ isError: true })
-            })
-    }
+  componentDidMount() {
+    axios
+      .get(
+        `https://raider.io/api/v1/raiding/raid-rankings?raid=${this.props.raid}&difficulty=mythic&region=world
+        `
+      )
+      .then(res => {
+        let datas = res.data.raidRankings;
+        datas.splice(3);
 
-    render() {
-        const { raid, reverse } = this.props
-        const {isError, ranking} = this.state
-        if (isError) {
-            return <p>Error !</p>
-        } else {
-            return (
-                <StyledCardHome className={`d-flex flex-column ${reverse ? "flex-sm-row-reverse" : "flex-sm-row"} text-center`}>
-                    <div className="col-lg-3">
-                        <CardImg src="https://via.placeholder.com/150" className="border border-dark rounded-lg" alt="" jsp />
-                    </div>
-                    <section className="col-lg-9">
-                        <h1>{raid}</h1>
-                        <h2>Small description of the raid</h2>
-                        {
-                            ranking.map((guild) => {
-                                return <p>{guild.guild.name}</p>
-                            })
-                        }
-                        <p>First ranked guild</p>
-                        <p>Second ranked guild</p>
-                        <p>third ranked guild</p>
-                    </section>
-                </StyledCardHome>
-            );
-        }
-    }
-};
+        this.setState({ ranking: datas });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ isError: true });
+      });
+  }
 
+  render() {
+    const { raid, reverse } = this.props;
+    const { isError, ranking } = this.state;
+    if (isError) {
+      return <p>Error !</p>;
+    } else {
+      return (
+        <StyledCardHome
+          className={`d-flex flex-column ${
+            reverse ? 'flex-sm-row-reverse' : 'flex-sm-row'
+          } text-center`}
+        >
+          <div className="col-lg-3">
+            <CardImg
+              src="https://via.placeholder.com/150"
+              className="border border-dark rounded-lg"
+              alt=""
+              jsp
+            />
+          </div>
+          <section className="col-lg-9">
+            <h1>{raid}</h1>
+            <h2>Small description of the raid</h2>
+            {ranking.map(guild => {
+              return <p>{guild.guild.name}</p>;
+            })}
+          </section>
+        </StyledCardHome>
+      );
+    }
+  }
+}
 
 export default CardHome;
--- src/components/WithRaids.jsx
+++ src/components/WithRaids.jsx
@@ -1,29 +1,23 @@
-import React from "react"
+import React from 'react';
 
-import CardHome from "./CardHome"
+import CardHome from './CardHome';
 
 class WithRaids extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            raids: [
-                "uldir",
-                "the-eternal-palace",
-                "battle-of-dazaralor",
-            ]
-        }
-    }
-    render() {
-        return (
-            <div>
-                {
-                    this.state.raids.map((raid, i) => {
-                        return <CardHome raid={raid} reverse={i % 2 === 0 ? true : false} />
-                    })
-                }
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      raids: ['uldir', 'the-eternal-palace', 'battle-of-dazaralor']
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.raids.map((raid, i) => {
+          return <CardHome raid={raid} reverse={i % 2 === 0 ? true : false} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default WithRaids;
