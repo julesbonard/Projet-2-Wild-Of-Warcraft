96cf9807ac546a3aea041884d076cf52ab373796
Fanny <fannette.6576@gmail.com>
2019-10-22 09:29:15 +0200

    Changement de fichier guild en fichier Rank

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,21 +1,19 @@
-import React from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from "./pages/Home"
-import Ranking from "./pages/Ranking"
-import Guild from "./pages/Guild"
-
+import Home from './pages/Home';
+import Ranking from './pages/Ranking';
+import Rank from './pages/Rank';
 function Router() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/ranking" component={Ranking} />
-        <Route path="/guild" component={Guild} />
+        <Route path="/Rank" component={Rank} />
       </Switch>
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default Router
+export default Router;
