5551e5c2626956357359486289d82a470dbdf76a
Jules Bonard <julestim.bonard@gmail.com>
2019-10-25 12:01:56 +0200

    create dynamique card for home, install SASS in project

--- src/components/StyleCardHome.jsx
+++ src/components/StyleCardHome.jsx
@@ -1,13 +1,19 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const StyledCardHome = styled.article`
-width: 80vw;
-height: auto;
-margin-left: 10vw;
-margin-top: 30px;
-padding-top: 20px;
-padding-bottom 20px;
-color: rgb(192,192,192)
+    margin: 50px 0;
+    overflow: hidden;
+    width: 370px;
+    background: #FFF;
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);
+        a{
+        color: inherit;
+        text-decoration: none;
+    }
+    &:hover{
+        box-shadow: 0px 0px 50px rgba(0, 0, 0, 0.3);
+    }
+}
 `;
 
 export default StyledCardHome;
