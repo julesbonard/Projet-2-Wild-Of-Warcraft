9c5dfe82fc93a798bca55038d8336ff86a47f229
Clara Desperben <cdesperben@yahoo.fr>
2019-11-05 16:33:28 +0100

    linked cardhome with cardguild fforr the first three guilds, created a state for the cardguild  component, added content on cardguild modulee css to add  style on first three guilds on homepage

--- src/components/CardGuild.jsx
+++ src/components/CardGuild.jsx
@@ -1,42 +1,43 @@
-// import React from 'react';
-// import '../Css/CardGuild.css';
-// import axios from 'axios';
+import React from 'react';
+import '../Css/CardGuild.module.css';
+import axios from 'axios';
 
-// class CardGuild extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       guildName: '',
-//       roster: [],
-//       isLoading: false
-//     };
-//   }
+class CardGuild extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      guildName: '',
+      roster: [],
+      isLoading: false
+    };
+  }
 
-//   componentDidMount() {
-//     this.setState({ isLoading: true });
-//     axios
-//       .get(
-//         `https://cors-anywhere.herokuapp.com/https://raider.io/api/guilds/${this.props.region}/${this.props.realm}/${this.props.guild}/roster`
-//       )
-//       .then(response => {
-//         console.log(response);
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://raider.io/api/guilds/${this.props.region}/${this.props.realm}/${this.props.guild}/roster`
+      )
+      .then(response => {
+        const roster = response.data.guildRoster.roster.filter(
+          player => player.raidProgress.progress.mythic > 4
+        );
+        console.log(roster);
 
-//         const roster = response.data.guildRoster.roster.filter(
-//           player => player.raidProgress.progress.mythic > 4
-//         );
-//         roster.splice(10);
-//         this.setState({ roster: roster });
-//       })
-//       .catch(err => console.log);
-//   }
+        this.setState({ roster: roster });
+      })
+      .catch(err => console.log(err));
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <h1>test</h1>
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        {this.state.roster.map(player => {
+          return <p>{player.character.name}</p>;
+        })}
+      </div>
+    );
+  }
+}
 
-// export default CardGuild;
+export default CardGuild;
