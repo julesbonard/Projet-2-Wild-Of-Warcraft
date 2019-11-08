765ccb2f07114facce34ac80281316adbc47f4be
Jules Bonard <julestim.bonard@gmail.com>
2019-11-08 14:09:03 +0100

    Add reactstrap card on aaaalllll the home page ! use module css and delete styled components for home card

--- src/components/home/Raid.jsx
+++ src/components/home/Raid.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { Col } from 'reactstrap';
+import { Col, Card, CardImg, CardBody, CardTitle, CardText, CardSubtitle } from 'reactstrap';
 
-import StyledRaid from './StyledRaid';
 import styles from './Raid.module.scss';
 
-const { card, bgColor, thumb, body, category, title, subtitle, desc } = styles;
+const { center, card, thumb, body, category, title, subtitle, desc } = styles;
 
 function Raid({ raid, icon }) {
   const [isError, setIsError] = useState(false);
@@ -29,41 +28,73 @@ function Raid({ raid, icon }) {
         });
     };
     fetchDatas();
-  }, []);
+  }, [raid]);
 
   if (isError) {
     return <div>Error !</div>;
   }
+
   return (
-    <Col sm="6" xl="3" lg="6">
-      <StyledRaid className={`${card} ${bgColor}`}>
+    <Col
+      xs={{ offset: 1, size: 9 }}
+      sm={{ offset: 0, size: 6 }}
+      lg={{ offset: 1, size: 5 }}
+      xl={{ offset: 0, size: 3 }}
+      className="mt-3"
+    >
+      <Card className={card}>
         <div className={thumb}>
-          <Link to={`/Rank/${raid}`}>
-            <img width="320px" height="235px" src={`/Image/${raid}.jpg`} alt={raid} />
-          </Link>
+          <CardImg src={`/Image/${raid}.jpg`} />
         </div>
-        <section className={body}>
-          <div class={category}>
-            <p>{icon}</p>
-          </div>
+        <CardBody className={body}>
+          <CardText className={category}>{icon}</CardText>
           <Link to={`/Rank/${raid}`} style={{ textDecoration: 'none' }}>
-            <h1 className={title}>{raid.replace(/-/g, ' ')}</h1>
+            <CardTitle className={title}>{raid.replace(/-/g, ' ')}</CardTitle>
           </Link>
-          <h2 className={subtitle}>Top 3 guilds :</h2>
+          <CardSubtitle className={subtitle}>Top 3 guilds :</CardSubtitle>
           {ranking.map(guild => {
             return (
-              <h6>
-                {/* balise p changée en h6 pour styliser les liens sur la page principale sans affecter le reste du code*/}
+              <CardText className={center}>
                 <Link to={guild.guild.path} className={desc}>
                   {guild.guild.name}
                 </Link>
-              </h6>
+              </CardText>
             );
           })}
-        </section>
-      </StyledRaid>
+        </CardBody>
+      </Card>
     </Col>
   );
+
+  // return (
+  //   <Col sm="6" xl="3" lg="3">
+  //     <article className={card}>
+  //       <div className={thumb}>
+  //         <Link to={`/Rank/${raid}`}>
+  //           <img width="320px" height="235px" src={`/Image/${raid}.jpg`} alt={raid} />
+  //         </Link>
+  //       </div>
+  //       <section className={body}>
+  //         <div class={category}>
+  //           <p>{icon}</p>
+  //         </div>
+  //         <Link to={`/Rank/${raid}`} style={{ textDecoration: 'none' }}>
+  //           <h1 className={title}>{raid.replace(/-/g, ' ')}</h1>
+  //         </Link>
+  //         <h2 className={subtitle}>Top 3 guilds :</h2>
+  //         {ranking.map(guild => {
+  //           return (
+  //             <h4>
+  //               <Link to={guild.guild.path} className={desc}>
+  //                 {guild.guild.name}
+  //               </Link>
+  //             </h4>
+  //           );
+  //         })}
+  //       </section>
+  //     </article>
+  //   </Col>
+  // );
 }
 
 export default Raid;
