9cacd4f9710f30cc9c3478455581c3a105c688ab
Clara Desperben <cdesperben@yahoo.fr>
2019-11-08 14:51:03 +0100

    linkeed raid page and guild page, changed color of elements on raid page

--- src/components/raid/GuildTopTen.jsx
+++ src/components/raid/GuildTopTen.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function RaidRank({ i, rank, guild }) {
+import './RaidPage.module.css';
+
+function GuildTopTen({ i, rank, guild }) {
   return (
     <div
       className={`player col-6 ${i % 2 === 0 ? 'text-right' : 'text-left'}`}
       style={{ color: 'white' }}
     >
       {rank}
-      {guild.name}
+      <Link to={guild.path}>{guild.name}</Link>
     </div>
   );
 }
 
-export default RaidRank;
+export default GuildTopTen;
